2413bb89cf84d1a851eb3655bd4292cd4ed5c801
Yuliia_Harashchenko <yharashchenko@ukr.net>
2024-06-04 15:03:58 +0300

    Homework 9

---
+++ src/js/2-form.js
@@ -0,0 +1,47 @@
+const formData = { email: '', message: '' };
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const textarea = form.querySelector('textarea');
+
+form.addEventListener('input', () => {
+  formData.email = form.elements.email.value.trim();
+  formData.message = form.elements.message.value.trim();
+  saveToLS(STORAGE_KEY, formData);
+});
+
+function saveToLS(key, value) {
+  const jsonData = JSON.stringify(value);
+  localStorage.setItem(key, jsonData);
+}
+
+function loadFromLS(key) {
+  const json = localStorage.getItem(key);
+  try {
+    const data = JSON.parse(json);
+    return data;
+  } catch {
+    return json;
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  const formData = loadFromLS(STORAGE_KEY);
+
+  form.elements.email.value = formData?.email || '';
+  form.elements.message.value = formData?.message || '';
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  } else console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  formData.email = '';
+  formData.message = '';
+  form.reset();
+});
